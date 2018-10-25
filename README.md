d235a1c41fb1ece94fe25faf60ddb79f03f6349d
David Carter <carteriffic@gmail.com>
2018-10-24 20:11:07 -0400

    connecting to MongoDB

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Using udemy material in 'MERN stack front to back' courseware as starting point
 npm init
 npm i express mongoose passport passport-jwt jsonwebtoken body-parser eslint bcryptjs validator
 npm i -D nodemon # devDependency
+
+create a config/keys.js file which will not go to repo due to .gitignore with contents like this.
+
+module.exports = {
+  mongoURI:
+    "mongodb://username:password@ds121311.mlab.com:21311/scenegrenade"
+};
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+// DB Config
+
+const db = require("./config/keys").mongoURI;
+
+// Connect to mongodb
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello world"));
 const port = process.env.PORT || 5000;
 
