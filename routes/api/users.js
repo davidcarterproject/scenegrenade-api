fc11b56bcca5e08582e525be4eeda86eb75c2154
David Carter <carteriffic@gmail.com>
2018-10-27 21:34:33 -0400

    add validation for register and login fields

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load User model
 
@@ -64,6 +65,12 @@ router.post("/register", (req, res) => {
 // @desc Login user
 // @access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json({ errors });
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -71,7 +78,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      res.status(404).json(errors);
     }
     // Check password
     bcrypt.compare(password, user.password).then(isMatch => {
@@ -91,7 +99,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
