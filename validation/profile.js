667394b8cdc130fddd7b7ce092ed03c03cf02edf
David Carter <carteriffic@gmail.com>
2018-11-10 15:52:29 -0500

    add provide validation

---
+++ validation/profile.js
@@ -0,0 +1,33 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.handle) ? data.handle : "";
+  data.company = !isEmpty(data.company) ? data.company : "";
+  data.website = !isEmpty(data.website) ? data.website : "";
+
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = "Handle needs to be between 2 and 40 characters long";
+  }
+
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = "Handle field is required";
+  }
+
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      errors.website = "Not a valid URL";
+    }
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.name = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
